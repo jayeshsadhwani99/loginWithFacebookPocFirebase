c88bb7c93e9ebe888b8459c0f9f8fc92729c7745
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-03-11 00:06:53 +0530

    Add scopes

--- public/init.js
+++ public/init.js
@@ -26,16 +26,21 @@ const app = initializeApp(firebaseConfig);
 console.log(app);
 
 const fbProvider = new FacebookAuthProvider();
+const fbScopeProvider = new FacebookAuthProvider();
 const googleProvider = new GoogleAuthProvider();
 const auth = getAuth();
 
 console.log(auth);
 
-let token = null;
 auth.languageCode = "it";
+
+fbScopeProvider.addScope(
+  "catalog_management, instagram_basic, instagram_content_publish, instagram_manage_insights, pages_manage_metadata, pages_messaging, pages_show_list, pages_user_gender, pages_user_locale, user_birthday, user_gender, instagram_graph_user_media, instagram_graph_user_profile"
+);
 
 var btn = document.getElementById("btn-login");
 var fbBtn = document.getElementById("btn-login-fb");
+var fbScopeBtn = document.getElementById("btn-login-fb-scope");
 var logoutBtn = document.getElementById("btn-logout");
 
 btn.addEventListener(
@@ -50,6 +55,14 @@ fbBtn.addEventListener(
   "click",
   function () {
     loginWithFacebook();
+  },
+  false
+);
+
+fbScopeBtn.addEventListener(
+  "click",
+  function () {
+    loginWithScope();
   },
   false
 );
@@ -96,6 +109,36 @@ function loginWithFacebook() {
       // ...
     });
 }
+
+const loginWithScope = () => {
+  signInWithPopup(auth, fbScopeProvider)
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user;
+
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+
+      console.log("User>>>>>", user);
+      console.log("AccessToken>>>>>", accessToken);
+
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = FacebookAuthProvider.credentialFromError(error);
+
+      console.log(error);
+
+      // ...
+    });
+};
 
 const login = () => {
   signInWithRedirect(auth, googleProvider);
